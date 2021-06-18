b8e75103ab617f55dd457295bbf2fdcf6e800868
Popovic-Daniel <77675542+Popovic-Daniel@users.noreply.github.com>
2021-06-18 11:21:02 +0200

    Update MainAppl.java

--- myTeam/src/pkgMain/MainAppl.java
+++ myTeam/src/pkgMain/MainAppl.java
@@ -7,5 +7,7 @@ public class MainAppl {
 	public static void main() {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("=== " + player.toString());
+		Player player2 = new Player(1, "Alaba", "Defense");
+		System.out.println("=== " + player2.toString());
 	}
 }
