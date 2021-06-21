644ac0ba3a12271f74a69457c1ea0877efd73c09
schueler <schueler@ubuntu>
2021-06-21 14:37:05 +0200

    add args argument to main

--- myTeam/src/pkgMain/MainAppl.java
+++ myTeam/src/pkgMain/MainAppl.java
@@ -4,7 +4,7 @@ import pkgData.Player;
 
 public class MainAppl {
 
-	public static void main() {
+	public static void main(String[] args) {
 		Player player = new Player(2, "Dragovic", "Defense");
 		System.out.println("=== " + player.toString());
 		Player player2 = new Player(1, "Alaba", "Defense");
